9c27175d41569ee065b6a692b2567dc995d0c8b6
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-09-24 21:08:12 +0200

    Show location search options

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,14 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { restartableTask, timeout } from 'ember-concurrency';
 
 export default class IndexController extends Controller {
   @tracked
   isPopupOpen = false;
 
-  address = '';
+  @tracked
+  searchResults = [];
 
   @service router;
 
@@ -25,5 +27,30 @@ export default class IndexController extends Controller {
   submitAddress() {
     this.closePopup();
     this.router.transitionTo('guidelines');
+  }
+
+  @restartableTask
+  *searchTask(query) {
+    yield timeout(300); // 300ms debounce
+
+    if (query) {
+      try {
+        const response = yield fetch(
+          `https://inventaris.onroerenderfgoed.be/erfgoedobjecten?tekst=${encodeURIComponent(query)}`,
+          { headers: { accept: 'application/json' } },
+        );
+        const results = yield response.json();
+        this.searchResults = results;
+      } catch (error) {
+        console.error('Search failed', error);
+      }
+    } else {
+      this.searchResults = [];
+    }
+  }
+
+  @action
+  selectResult(result) {
+    console.log('Selected ID:', result.id);
   }
 }
