8221f797f4070bfdaa03e5507f3e8ec23debc853
Claire Lovisa <lovisa.claire@gmail.com>
2024-09-24 15:33:27 +0200

    Load guidelines from the model

--- app/routes/guidelines.js
+++ app/routes/guidelines.js
@@ -1,3 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class GuidelinesRoute extends Route {}
+export default class GuidelinesRoute extends Route {
+  @service store;
+
+  async model(param) {
+    // TODO use param
+    const mockUri = "https://id.erfgoed.net/aanduidingsobjecten/14969";
+
+    let annotations = await this.store.query('annotation', {
+      filter: {
+        'resource': mockUri
+      },
+    });
+
+    return annotations;
+  }
+}
