02903b25fcae923f3547f6ec0a26b582064f9698
Claire Lovisa <lovisa.claire@gmail.com>
2024-09-25 08:51:02 +0200

    Load annotations from designation object URI

--- app/routes/guidelines.js
+++ app/routes/guidelines.js
@@ -17,16 +17,12 @@ export default class GuidelinesRoute extends Route {
     if (!response.ok) throw new Error('Failed to fetch designation object');
 
     const designationObject = await response.json();
-    console.log('designation object uri:', designationObject.uri);
 
     // DESIGNATION OBJECT ANNOTATIONS
 
-    // TODO use designationObjectId
-    const mockUri = 'https://id.erfgoed.net/aanduidingsobjecten/14969';
-
     const annotations = await this.store.query('annotation', {
       filter: {
-        resource: mockUri,
+        resource: designationObject.uri,
       },
     });
 
